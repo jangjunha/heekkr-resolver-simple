0c91567c4931a20ee7624029f4ce30e77d34c663
jangjunha <hi@jangjunha.me>
2023-08-20 16:01:11 +0900

    단일화된 resolver에 맞게 CLI 수정

--- cli.py
+++ cli.py
@@ -1,13 +1,12 @@
 import argparse
 import asyncio
-import importlib
 
-from heekkr.resolver_pb2 import SearchRequest
-from heekkr.resolver_pb2_grpc import ResolverServicer
+from heekkr.resolver_pb2 import GetLibrariesRequest, SearchRequest
+
+from app import Resolver
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-r", "--resolvers", action="append", required=True)
 subparsers = parser.add_subparsers(title="command", dest="command", required=True)
 
 parser_libraries = subparsers.add_parser("libraries")
@@ -19,24 +18,19 @@ parser_search.add_argument("-l", "--library-ids", action="append", required=True
 
 async def main():
     args = parser.parse_args()
-    resolvers: list[ResolverServicer] = [
-        getattr(importlib.import_module(f".{rs}", "app.resolvers"), "Resolver")()
-        for rs in args.resolvers
-    ]
+    resolver = Resolver()
 
     match args.command:
         case "libraries":
-            for resolver in resolvers:
-                resp = await resolver.GetLibraries(None, None)
-                for lib in resp.libraries:
-                    print(f"{lib.id:16s} : {lib.name}")
+            resp = await resolver.GetLibraries(GetLibrariesRequest(), None)
+            for lib in resp.libraries:
+                print(f"{lib.id:16s} : {lib.name}")
         case "search":
-            for resolver in resolvers:
-                async for res in resolver.Search(
-                    SearchRequest(library_ids=args.library_ids, term=args.keyword),
-                    None,
-                ):
-                    print(res)
+            async for res in resolver.Search(
+                SearchRequest(library_ids=args.library_ids, term=args.keyword),
+                None,
+            ):
+                print(res)
 
 
 asyncio.run(main())
